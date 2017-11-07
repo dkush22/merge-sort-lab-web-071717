a0b2b0cd75d000b2b8ae8a47d8003aa816c3acd1
dkush22 <dkush22@gmail.com>
2017-11-07 17:58:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+function findMinAndRemoveSorted(array) {
 
+let min = array[0]
+let minIndex
+for (let i = 0; i < array.length; i++) {
+	if (array[i] < min) {
+		min = array[i]
+		minIndex = i
+	}
+}
+array.splice(minIndex, 1)
+return min
+}
+
+function findMinAndRemove(firstArray, secondArray) {
+	let minFirst = firstArray[0]
+	let minSecond = secondArray[0]
+
+	if (minFirst < minSecond) {
+		return firstArray.shift() 
+	}
+	else {
+		return secondArray.shift()
+	}
+}
+
+
+function merge(firstArray, secondArray) {
+let sorted = []
+let currentMin
+while (firstArray.length != 0 && secondArray.length != 0) {
+	let currentMin = findMinAndRemove(firstArray, secondArray)
+	sorted.push(currentMin)
+}
+return sorted.concat(firstArray).concat(secondArray)
+}
+
+
+
+function mergeSort(array) {
+let midpoint = (array.length / 2)
+let firstHalf = array.slice(0, midpoint)
+let secondHalf = array.slice(midpoint, array.length)
+
+if (array.length < 2) {
+	return array
+} else {
+	return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+}
+}
